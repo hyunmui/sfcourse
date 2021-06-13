178cf1fa9492c50298ac16bc89ee2233c188c46b
Hyeonmin Seon <hyunmui@outlook.kr>
2021-06-13 17:16:30 +0900

    1:52:30 orm complete

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * 
@@ -17,19 +18,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     /**
+     * 
+     * @var PostRepository
+     */
+    private $postRepository;
+
+    /**
+     * Constructor
+     * 
+     * @author Martin Seon
+     * @param PostRepository $postRepository 
+     * @return void 
+     */
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    /**
      * @Route("/", name="index")
+     * @return Response 
      */
     public function index(): Response
     {
+        $posts = $this->postRepository->findAll();
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts
         ]);
     }
 
     /**
      * @Route("/create", name="create")
      */
-    public function create(Request $request): Response
+    public function create(): Response
     {
         // create a new post with title
         $post = new Post();
@@ -43,5 +65,34 @@ class PostController extends AbstractController
 
         // return $this->render('post/create.html.twig', []);
         return new Response('Post was created!');
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @return Response 
+     */
+    public function show(int $id): Response
+    {
+        $post = $this->postRepository->find($id);
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @return Response 
+     */
+    public function remove(int $id): Response
+    {
+        $post = $this->postRepository->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash('success', 'Post was removed');
+
+        return $this->redirect($this->generateUrl('post.index'));
     }
 }
